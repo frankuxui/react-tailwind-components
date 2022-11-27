e098008614137f65b536a6ffbd57a45c5eaf6054
frankuxui <fisdray@gmail.com>
2022-11-27 01:22:47 +0100

    ui-fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import { Roboto } from '@next/font/google'
 import '../styles/globals.css'
 import '../styles/prims-synthWave84.css'
 import APPContext from 'components/docs/context'
 
-const roboto = Roboto({
-  weight: ['300', '400', '500', '700', '900'],
-  subsets: ['latin']
-})
-
 function MyApp ({ Component, pageProps }) {
   return (
     <>
-      <style jsx global>{` html { font-family: ${roboto.style.fontFamily}; } `}</style>
       <APPContext>
         <Component {...pageProps} />
       </APPContext>
