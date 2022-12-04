c08c978cac2c58967cbb3348a0c75a348842e43c
frankuxui <fisdray@gmail.com>
2022-12-04 03:06:27 +0100

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,16 @@ import React from 'react'
 import '../styles/globals.css'
 import '../styles/prims-synthWave84.css'
 import APPContext from 'components/docs/context'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp ({ Component, pageProps }) {
   return (
-    <APPContext>
-      <Component {...pageProps} />
-    </APPContext>
+    <>
+      <APPContext>
+        <Component {...pageProps} />
+      </APPContext>
+      <Analytics />
+    </>
   )
 }
 
